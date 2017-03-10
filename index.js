d5ab219210b1cc94405ddbf5454d280fa91cfca2
Michiel <michiel@blue-donut.com>
2017-03-10 23:46:29 +0100

    Please answer in channel with scheduler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Promise = require('promise');
+var schedule = require('node-schedule');
 
 const config = require('./config.js');
 const token = config('token');
@@ -10,11 +11,28 @@ var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var rtm = new RtmClient(token);
 
+const answers = {};
+
+addAnswer = function addAnswer(user, answer) {
+  console.log('addAnswer');
+  answers[user].push(answer);
+  console.log('answers', answers);
+}
+
+clearAnswers = function clearAnswers() {
+  console.log('clearAnswers');
+  config('users').forEach(function (user) {
+    answers[user.id] = [];
+  });
+  console.log('answers', answers);
+}
+
 getUsers = function getUsers () {
   return config('users'); 
 }
 
 getUserByName = function getUserByName (name, users) {
+  console.log('getUserByName');
   const filteredUsers = users.filter(function (user) {
     return user.name === name;
   });
@@ -25,10 +43,29 @@ getUserByName = function getUserByName (name, users) {
   }
 }
 
+getChannelIdByUserId = function getChannelIdByUserId (userId, users) {
+  const filteredUsers = users.filter(function (user) {
+    return user.id === userId;
+  });
+  if (filteredUsers.length === 1) {
+    return filteredUsers[0].channel;
+  } else {
+    return null;
+  }
+}
 
+isBottest = function isBottest (channel) {
+  return channel === 'G4G5TRTHP';
+}
+
+var web = new WebClient(token);
+/*
+web.groups.list(function (err, res) {
+  console.log(err, res);
+});
+*/
 
 /*
-var web = new WebClient(token);
 web.users.list(function (err, res) {
   if (err === undefined && res.ok) {
     console.log(res);
@@ -44,13 +81,12 @@ web.users.list(function (err, res) {
 });
 */
 
-sendDM = function sendDM(name, text ) {
-  const user = getUserByName(name, getUsers());
-  if (user !== null) {
-    rtm.sendMessage(text, user.channel);
-  }
+sendDM = function sendDM(channelId, text ) {
+  console.log('sendDM', text);
+  rtm.sendMessage(text, channelId);
 };
 
+clearAnswers();
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
@@ -58,11 +94,13 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
 
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-  sendDM('michiel', 'hoi!');
+  onConnectionOpened();
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-    console.log('Message:', message);
+  if (isBottest(message.channel)) {
+    addAnswer(message.user, message.text);
+  }
 });
 
 rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
@@ -74,3 +112,22 @@ rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) 
 });
 
 rtm.start();
+
+onConnectionOpened = function onConnectionOpened () {
+  console.log('onConnectionOpened');
+  var j = schedule.scheduleJob('0 * * * * *', function(){
+    clearAnswers();
+  });
+
+  var k = schedule.scheduleJob('50 * * * * *', function(){
+    console.log('Check answers');
+    config('users').forEach(function (user) {
+      console.log('user', user);
+      if (answers[user.id].length === 0) {
+        sendDM(getChannelIdByUserId(user.id, getUsers()), 'Please answer.');
+      } else {
+        sendDM(getChannelIdByUserId(user.id, getUsers()), 'Thanks for answering!');
+      }
+    });
+  });
+}
